9f2aa594dedd5d2ecfc48a9ee161c29f91dbdddd
ATSUATSUATSU <atsukymsh1@yahoo.co.jp>
2021-10-05 21:21:48 +0900

    update

--- scraiping.py
+++ scraiping.py
@@ -2,3 +2,18 @@ import requests
  
 webpage_response = requests.get('https://www.goldmansachs.com/')
 print(webpage_response.text)
+
+
+
+
+import requests
+from bs4 import BeautifulSoup
+
+##webpage_response = requests.get('https://content.codecademy.com/courses/beautifulsoup/shellter.html')
+webpage_response = requests.get('https://www.goldmansachs.com/')
+
+
+webpage = webpage_response.content
+soup = BeautifulSoup(webpage, "html.parser")
+
+print(soup)
